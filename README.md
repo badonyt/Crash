191a8160a8898837f056dce5ec1b27e48a88fd67
badonyt <soccerskin87@gmail.com>
2022-06-20 21:10:49 +0100

    Added so there is a new token mechanism

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Crash
-This is a bot from my yt video check it out (:, its in portuguese 
+
+This is a bot from my yt video check it out (:, its in portuguese
 
 # Commands
+
 -delete to delete a text channel
 
 -add "text channel" and it will add 10 channels of the thing you put
+
+# Setup
+
+You need to have node installed, npm install discord.js and https
+
+And also create a file called config.json
+
+and insert inside the config.json
+
+{
+"token": "YOUR-BOT-TOKEN"
+}
+
+# Updates coming soon
+
+We have updates coming soon!
--- main.js
+++ main.js
@@ -1,39 +1,40 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const prefix = '-';
+const prefix = "-";
 
-const fs = require('fs');
+const fs = require("fs");
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+const commandFiles = fs
+	.readdirSync("./commands/")
+	.filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command);
+	client.commands.set(command.name, command);
 }
 
-
-client.once('ready', () => {
-    console.log('crash is online!');
+client.once("ready", () => {
+	console.log("crash is online!");
 });
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
-    }else if (command === "delete"){
-        client.commands.get('delete').execute(message, args);
-    }else if (command === "add"){
-        client.commands.get('add').execute(message, args);
-    }else if (command === "clear")[
-        client.commands.get("clear").execute(message, args)
-    ]
+	if (command === "ping") {
+		client.commands.get("ping").execute(message, args);
+	} else if (command === "delete") {
+		client.commands.get("delete").execute(message, args);
+	} else if (command === "add") {
+		client.commands.get("add").execute(message, args);
+	} else if (command === "clear")
+		[client.commands.get("clear").execute(message, args)];
 });
 
-client.login('token (:');
+const configs = require("./config.json");
+client.login(configs.token);
